02c34e7f6bd5e12bda4d89842711238d01d246af
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-08 02:15:02 -0300

    Agregue validaciones html y javascript al input ubicacion

--- src/components/FormularioClima.jsx
+++ src/components/FormularioClima.jsx
@@ -1,15 +1,41 @@
 import { Form, InputGroup, Button } from "react-bootstrap";
 import CardClima from "./CardClima";
+import { useState } from "react";
 
 const FormularioClima = () => {
+  const [ubicacion, setUbicacion] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (ubicacion.trim() !== "") {
+      console.log(ubicacion);
+    } else {
+      alert("Debes ingresar una ubicacion valida");
+    }
+
+    setUbicacion("");
+  };
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <InputGroup className="mb-3">
           <InputGroup.Text>
             <i className="bi bi-geo-alt-fill"></i>
           </InputGroup.Text>
-          <Form.Control placeholder="Ubicación" aria-label="Ubicación" />
+          <Form.Control
+            type="text"
+            placeholder="Ubicación"
+            aria-label="Ubicación"
+            value={ubicacion}
+            pattern="^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$"
+            onChange={(e) => {
+              setUbicacion(e.target.value);
+            }}
+            minLength="3"
+            maxLength="25"
+            required
+          />
           <Button variant="primary" type="submit">
             <i className="bi bi-search"></i>
           </Button>
